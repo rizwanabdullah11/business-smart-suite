2850a2384d733bf945abcfa5e1a7b30d4eb1ed83
rizwanabdullah11 <rizwan.abdullah193@gmail.com>
2026-02-21 00:57:37 +0500

    here I remove all components

--- app/coshh/add-review-dialog.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-// Placeholder for Add Review Dialog
-// This component will be implemented with API calls instead of Prisma
-
-export default function AddReviewDialog({ coshhId }: { coshhId: string }) {
-    return null
-}
--- app/statement-of-applicability/add-review-dialog.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-// Placeholder for Add Review Dialog
-// This component will be implemented with API calls instead of Prisma
-
-export function AddReviewDialog({ controlId }: { controlId: string }) {
-    return null
-}
